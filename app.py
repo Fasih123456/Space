1b60788685462dc27288093df79d8533a61c6127
Fasih123456 <fasihislam1999@gmail.com>
2020-06-01 22:03:12 -0300

    Testing commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ pygame.init()
 
 # create the screen of 800x600
 screen = pygame.display.set_mode((800, 600))
-
+screen = pygame.display.set_mode((800, 600))
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
